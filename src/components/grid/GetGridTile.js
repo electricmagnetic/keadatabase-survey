ed238d6a942756cf91c980d7c800cd6ac1fa15c1
George Moon <george@moon.org.nz>
2019-06-19 15:32:15 +1200

    Get Grid Tile tool now uses API

--- src/components/grid/GetGridTile.js
+++ src/components/grid/GetGridTile.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
+import { connect } from 'react-refetch';
 import PropTypes from 'prop-types';
 
-import tile_placeholder from './tile_placeholder.png';
+import Loader from '../helpers/Loader';
+import Error from '../helpers/Error';
+
+import './GetGridTile.css';
+
+const GridTileView = ({ data }) => {
+  return (
+    <div className="row">
+      <div className="col-lg-7 order-lg-2 mb-3">
+        <dl>
+          <dt>Grid ID</dt>
+          <dd>{data.id}</dd>
+          <dt>Lower Left (NZTM)</dt>
+          <dd>
+            {data.min.coordinates[0]}, {data.min.coordinates[1]}
+          </dd>
+          <dt>Upper Right (NZTM)</dt>
+          <dd>
+            {data.max.coordinates[0]}, {data.max.coordinates[1]}
+          </dd>
+          <dt>Source</dt>
+          <dd>Land Information New Zealand, licensed for reuse under the CC BY 4.0.</dd>
+        </dl>
+      </div>
+      <div className="col-lg-5 order-lg-1">
+        <img src={data.get_image} alt="Map tile" className="img-fluid img-full" />
+      </div>
+    </div>
+  );
+};
 
 class GetGridTile extends Component {
   constructor(props) {
     super(props);
     this.state = {
       value: '',
-      tile: tile_placeholder,
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -20,57 +49,44 @@ class GetGridTile extends Component {
   }
 
   handleSubmit(event) {
-    var url = `https://geo.keadatabase.nz/tiles/${this.state.value}.png`;
+    // Disallow fetching without a value (so it doesn't try fetch all grid tiles)
+    if (this.state.value) this.props.lazyFetchGridTile(this.state.value);
 
-    this.setState({
-      tile: url,
-    });
     event.preventDefault();
   }
 
-  defaultToPlaceholder(event) {
-    event.target.src = tile_placeholder;
-  }
-
   render() {
+    const gridTileFetch = this.props.gridTileFetch;
+
     return (
       <div className="GetGridTile">
-        <div className="row">
-          <div className="col-lg-7 order-lg-2 mb-3">
-            <form onSubmit={this.handleSubmit} className="form-inline d-print-none mb-3">
-              <div className="input-group">
-                <label htmlFor="id" className="sr-only">
-                  Grid ID:
-                </label>
-                <input
-                  className="form-control"
-                  id="id"
-                  type="text"
-                  value={this.state.value}
-                  onChange={this.handleChange}
-                  maxLength="7"
-                  placeholder="Grid ID"
-                />
-                <div className="input-group-append">
-                  <input type="submit" value="Get Tile" className="btn btn-primary" />
-                </div>
-              </div>
-            </form>
-            <dl>
-              <dt className="d-none d-print-block">Grid ID</dt>
-              <dd className="d-none d-print-block">{this.state.value}</dd>
-              <dt>Source</dt>
-              <dd>Land Information New Zealand, licensed for reuse under the CC BY 4.0.</dd>
-            </dl>
-          </div>
-          <div className="col-lg-5 order-lg-1">
-            <img
-              onError={this.defaultToPlaceholder}
-              src={this.state.tile}
-              alt="Map tile"
-              className="img-fluid"
+        <form onSubmit={this.handleSubmit} className="form-inline d-print-none mb-3">
+          <div className="input-group">
+            <label htmlFor="id" className="sr-only">
+              Grid ID:
+            </label>
+            <input
+              className="form-control"
+              id="id"
+              type="text"
+              value={this.state.value}
+              onChange={this.handleChange}
+              maxLength="7"
+              placeholder="Grid ID (XXXX-XX)"
             />
+            <div className="input-group-append">
+              <input type="submit" value="Get Tile" className="btn btn-primary" />
+            </div>
           </div>
+        </form>
+        <div className="result">
+          {gridTileFetch.pending ? (
+            <Loader />
+          ) : gridTileFetch.rejected ? (
+            <Error message="Tile ID invalid" />
+          ) : (
+            gridTileFetch.fulfilled && <GridTileView data={gridTileFetch.value} />
+          )}
         </div>
       </div>
     );
@@ -79,7 +95,15 @@ class GetGridTile extends Component {
 
 GetGridTile.propTypes = {
   value: PropTypes.string,
-  tile: PropTypes.string,
+  gridTileFetch: PropTypes.object,
 };
 
-export default GetGridTile;
+GetGridTile.defaultProps = {
+  gridTileFetch: {},
+};
+
+export default connect(props => ({
+  lazyFetchGridTile: id => ({
+    gridTileFetch: `https://data.keadatabase.nz/surveys/grid_tiles/${id}/`,
+  }),
+}))(GetGridTile);
---
+++ src/components/helpers/Error.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './Error.css';
+
+const Error = ({ message, children }) => {
+  return (
+    <div className="Error alert alert-primary" role="alert">
+      <p className="m-0">{message}</p>
+      {children}
+    </div>
+  );
+};
+
+Error.propTypes = {
+  message: PropTypes.string.isRequired,
+  children: PropTypes.node,
+};
+
+Error.defaultProps = {
+  message: 'Error',
+};
+
+export default Error;

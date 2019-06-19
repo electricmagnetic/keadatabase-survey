46ab215205c2d64de82355c9ceb2a669fc04c7e3
George Moon <george@moon.org.nz>
2019-06-19 15:32:59 +1200

    Adjusting map to use local tiles

--- src/components/grid/GridTileMap.js
+++ src/components/grid/GridTileMap.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import { connect } from 'react-refetch';
 import { GeoJSON, LayersControl, ScaleControl } from 'react-leaflet';
 
-import Loader from '../helpers/Loader';
 import Map from '../map/Map';
 
 import './GridTileMap.css';
+
+import gridTiles from '../../assets/geo/tiles.json';
 
 class GridTileMap extends Component {
   gridTileOnEachFeature(feature, layer) {
@@ -24,41 +24,28 @@ class GridTileMap extends Component {
   }
 
   render() {
-    const { gridTilesFetch } = this.props;
-
-    if (gridTilesFetch.pending) {
-      return <Loader />;
-    } else if (gridTilesFetch.rejected) {
-      return <span>Error</span>;
-    } else if (gridTilesFetch.fulfilled) {
-      const data = gridTilesFetch.value;
-      return (
-        <div className="GridTileMap">
-          <Map>
-            <LayersControl position="topright" collapsed={false}>
-              <LayersControl.Overlay name="<strong>Grid Tiles</strong>" checked>
-                <GeoJSON
-                  data={data}
-                  onEachFeature={this.gridTileOnEachFeature}
-                  style={{
-                    color: '#222222',
-                    weight: 2,
-                    opacity: 0.6,
-                    fillOpacity: 0,
-                  }}
-                />
-              </LayersControl.Overlay>
-            </LayersControl>
-            <ScaleControl />
-          </Map>
-        </div>
-      );
-    }
+    return (
+      <div className="GridTileMap">
+        <Map>
+          <LayersControl position="topright" collapsed={false}>
+            <LayersControl.Overlay name="<strong>Grid Tiles</strong>" checked>
+              <GeoJSON
+                data={gridTiles}
+                onEachFeature={this.gridTileOnEachFeature}
+                style={{
+                  color: '#222222',
+                  weight: 2,
+                  opacity: 0.6,
+                  fillOpacity: 0,
+                }}
+              />
+            </LayersControl.Overlay>
+          </LayersControl>
+          <ScaleControl />
+        </Map>
+      </div>
+    );
   }
 }
 
-// https://geo.keadatabase.nz/grid/4326.geojson
-
-export default connect(props => ({
-  gridTilesFetch: `https://data.keadatabase.nz/geojson/grid_tiles/?page_size=6944`,
-}))(GridTileMap);
+export default GridTileMap;

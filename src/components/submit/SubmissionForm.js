040687e9c718330c5ad68fdfe035d6959b84e718
George Moon <george@moon.org.nz>
2019-06-25 15:04:07 +1200

    Added redirect on success

--- src/components/submit/SubmissionForm.js
+++ src/components/submit/SubmissionForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-refetch';
 import { Form, withFormik } from 'formik';
+import { withRouter } from 'react-router-dom';
 
 import Loader from '../helpers/Loader';
 import Error from '../helpers/Error';
@@ -18,20 +19,22 @@ class FormComponent extends Component {
     // Handle react-refetch response (either successful POST or error handling for fields)
     if (this.props.postSubmissionResponse) {
       const { postSubmissionResponse } = this.props;
+      const isSettled =
+        postSubmissionResponse.settled &&
+        prevProps.postSubmissionResponse.settled !== postSubmissionResponse.settled;
 
       // Conclude isSubmitting if either rejected or fulfilled
-      if (postSubmissionResponse.rejected && this.props.isSubmitting)
-        this.props.setSubmitting(false);
-      if (postSubmissionResponse.fulfilled && this.props.isSubmitting)
+      if (
+        (postSubmissionResponse.rejected || postSubmissionResponse.fulfilled) &&
+        this.props.isSubmitting
+      )
         this.props.setSubmitting(false);
 
-      // Set formik status if API errors encountered
-      if (postSubmissionResponse.rejected) {
-        // Only set status one time, after react-refetch has finished
-        if (prevProps.postSubmissionResponse.settled !== postSubmissionResponse.settled) {
-          this.props.setStatus(postSubmissionResponse.reason.cause);
-        }
-      }
+      // Set formik status if API errors encountered, otherwise redirect to success page
+      if (postSubmissionResponse.rejected && isSettled)
+        this.props.setStatus(postSubmissionResponse.reason.cause);
+      else if (postSubmissionResponse.fulfilled && isSettled)
+        this.props.history.push(`/submit/success/${postSubmissionResponse.value.id}`);
     }
   }
 
@@ -42,7 +45,7 @@ class FormComponent extends Component {
     else if (submissionOptions.rejected)
       return (
         <Error message="Error">
-          {submissionOptions.reason.cause.detail && `(${submissionOptions.reason.cause.detail})`}
+          {submissionOptions.reason.cause && `(${submissionOptions.reason.cause.detail})`}
         </Error>
       );
     else if (submissionOptions.fulfilled) {
@@ -70,16 +73,18 @@ const SubmissionForm = withFormik({
   },
 })(FormComponent);
 
-export default connect(props => ({
-  submissionOptions: {
-    url: API_URL,
-    method: 'OPTIONS',
-  },
-  postSubmission: values => ({
-    postSubmissionResponse: {
+export default withRouter(
+  connect(props => ({
+    submissionOptions: {
       url: API_URL,
-      method: 'POST',
-      body: JSON.stringify(values),
+      method: 'OPTIONS',
     },
-  }),
-}))(SubmissionForm);
+    postSubmission: values => ({
+      postSubmissionResponse: {
+        url: API_URL,
+        method: 'POST',
+        body: JSON.stringify(values),
+      },
+    }),
+  }))(SubmissionForm)
+);

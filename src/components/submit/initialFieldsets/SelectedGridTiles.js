c63b2143e8147d998e4cad78e94d6203997f7c62
George Moon <george@moon.org.nz>
2019-07-14 14:53:18 +1200

    Improving consistency

--- src/components/submit/initialFieldsets/SelectedGridTiles.js
+++ src/components/submit/initialFieldsets/SelectedGridTiles.js
@@ -1,29 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Error from '../../helpers/Error';
 import GridTile from '../../grid/GridTile';
 
 /**
   Displays a given set of gridTile IDs as a grid (without additional details).
  */
-const SelectedGridTiles = ({ gridTiles }) => (
-  <div className="SelectedGridTiles card bg-faded mb-3">
-    <div className="card-body my-n3">
-      <div className="form-row">
-        {gridTiles.map(gridTile => (
-          <div className="col-6 col-md-4" key={gridTile}>
-            <GridTile id={gridTile} hideDetails type="card" />
+const SelectedGridTiles = ({ gridTiles }) => {
+  if (gridTiles.length > 0) {
+    return (
+      <div className="SelectedGridTiles card bg-faded mb-3">
+        <div className="card-body my-n3">
+          <div className="form-row">
+            {gridTiles.map(gridTile => (
+              <div className="col-6 col-md-4" key={gridTile}>
+                <GridTile id={gridTile} hideDetails type="card" />
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
-      {gridTiles.length === 0 && (
-        <p className="my-3">
-          <em>No tiles selected.</em>
-        </p>
-      )}
-    </div>
-  </div>
-);
+    );
+  } else {
+    return <Error message="No grid tile selected" info />;
+  }
+};
 
 SelectedGridTiles.propTypes = {
   gridTiles: PropTypes.array.isRequired,

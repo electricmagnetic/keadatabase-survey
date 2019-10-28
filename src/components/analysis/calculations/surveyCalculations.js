c3f3c024921ea7eea316ce7b3fa304dc66cdf05a
George Moon <george@moon.org.nz>
2019-10-28 16:35:48 +0100

    Survey hour updates

--- src/components/analysis/calculations/surveyCalculations.js
+++ src/components/analysis/calculations/surveyCalculations.js
@@ -2,4 +2,4 @@
   Calculate number of kea encounters per total hours surveyed
  */
 export const calculateEncounterRate = surveyAnalysis =>
-  Math.round((surveyAnalysis.hours_with_kea / surveyAnalysis.hours_surveyed) * 100);
+  Math.round((surveyAnalysis.hours_total.with_kea / surveyAnalysis.hours_total.surveyed) * 100);

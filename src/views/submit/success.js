a99a5d862118ea397ba86b58e815a1e65d6c58ee
George Moon <george@moon.org.nz>
2019-12-19 11:44:27 +0100

    Adding text block to successful submission page

--- src/views/submit/success.js
+++ src/views/submit/success.js
@@ -3,6 +3,7 @@ import Helmet from 'react-helmet';
 import { Link } from 'react-router-dom';
 
 import Banner from '../../components/presentation/Banner';
+import Page from '../../components/presentation/Page';
 
 const SubmissionSuccessPage = ({ match }) => {
   const slug = match.params.slug;
@@ -10,18 +11,27 @@ const SubmissionSuccessPage = ({ match }) => {
   return (
     <div className="SubmissionSuccessPage">
       <Helmet title="Survey Submitted" />
-      <Banner size="small" additionalClasses="mb-3">
-        <h1 className="mb-3">Thanks!</h1>
-        <p className="lead">Your survey {slug && `(#${slug})`} has been successfully submitted.</p>
-        <Link to="/submit" className="btn btn-primary mr-3" role="button">
-          Report Another
-        </Link>
-        {slug && (
-          <Link to={'/surveys/' + slug} className="btn btn-light mr-3" role="button">
-            View Survey
+      <section className="mb-5">
+        <Banner size="small" additionalClasses="mb-3">
+          <h1 className="mb-3">Thanks!</h1>
+          <p className="lead">
+            Your survey {slug && `(#${slug})`} has been successfully submitted.
+          </p>
+          <Link to="/submit" className="btn btn-primary mr-3" role="button">
+            Report Another
           </Link>
-        )}
-      </Banner>
+          {slug && (
+            <Link to={'/surveys/' + slug} className="btn btn-light mr-3" role="button">
+              View Survey
+            </Link>
+          )}
+        </Banner>
+      </section>
+      <section className="mb-5">
+        <div className="container">
+          <Page id={556} />
+        </div>
+      </section>
     </div>
   );
 };

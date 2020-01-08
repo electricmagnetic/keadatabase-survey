499a680c058f8603251050b9e88c9a705e94093b
George Moon <george@moon.org.nz>
2020-01-08 11:20:46 +0100

    Added Topo250 tiles to BaseMap

--- src/components/map/BaseMap.js
+++ src/components/map/BaseMap.js
@@ -26,17 +26,23 @@ class BaseMap extends Component {
         className="map"
         viewport={this.state.viewport}
         minZoom={7}
-        maxZoom={14}
+        maxZoom={13}
         {...this.props}
       >
         <TileLayer
           attribution="Mapbox"
           url={`https://api.mapbox.com/styles/v1/mapbox/outdoors-v9/tiles/256/{z}/{x}/{y}?access_token=${process.env.REACT_APP_MAPBOX_API_KEY}`}
+          maxZoom={9}
+        />
+        <TileLayer
+          url={`https://tiles-a.data-cdn.linz.govt.nz/services;key=${process.env.REACT_APP_LINZ_API_KEY}/tiles/v4/layer=50798/EPSG:3857/{z}/{x}/{y}.png`}
+          minZoom={10}
+          maxZoom={11}
         />
         <TileLayer
           url={`https://tiles-a.data-cdn.linz.govt.nz/services;key=${process.env.REACT_APP_LINZ_API_KEY}/tiles/v4/layer=50767/EPSG:3857/{z}/{x}/{y}.png`}
           attribution="LINZ, licensed for reuse (CC BY 4.0)."
-          minZoom={11}
+          minZoom={12}
         />
         {children}
       </LeafletMap>

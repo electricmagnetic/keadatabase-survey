ad266e156757fbfebc3435dfc65135cdebeb31a2
George Moon <george@moon.org.nz>
2021-01-12 15:20:23 +1300

    First single-item converted to SWR

--- src/components/grid/GridTile.js
+++ src/components/grid/GridTile.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { connect } from 'react-refetch';
+import useSWR from 'swr';
 import PropTypes from 'prop-types';
 
 import GridTilePage from './GridTile/GridTilePage';
@@ -14,56 +14,43 @@ import tiles from '../../assets/geo/tiles.json';
 const API_URL = `${process.env.REACT_APP_API_BASE}/surveys/grid_tiles/`;
 
 /**
+  Selects component to render with, based on type
+*/
+const RenderGridTile = ({ gridTile, type, ...others }) => {
+  if (!gridTile) return <Error message="Tile ID invalid" />;
+
+  switch (type) {
+    case 'card':
+      return <GridTileCard gridTile={gridTile} {...others} />;
+    case 'item':
+      return <GridTileItem gridTile={gridTile} {...others} />;
+    default:
+      return <GridTilePage gridTile={gridTile} {...others} />;
+  }
+};
+
+/**
   Displays a grid tile using a local source, a fetch or a provided object.
 */
-class GridTile extends Component {
-  constructor(props) {
-    super(props);
-    this.renderGridTile = this.renderGridTile.bind(this);
-  }
+const GridTile = ({ id, gridTile, api, ...others }) => {
+  const { data, error, isValidating } = useSWR(api && id ? `${API_URL}${id}/` : null, {
+    dedupingInterval: 0,
+  });
 
-  renderGridTile(gridTile) {
-    // Allowing for an invalid grid tile from local source
-    if (!gridTile) return <Error message="Tile ID invalid" />;
-
-    const { type, ...others } = this.props;
-
-    switch (this.props.type) {
-      case 'card':
-        return <GridTileCard gridTile={gridTile} {...others} />;
-      case 'item':
-        return <GridTileItem gridTile={gridTile} {...others} />;
-      default:
-        return <GridTilePage gridTile={gridTile} {...others} />;
-    }
-  }
-
-  componentDidMount() {
-    if (this.props.api && this.props.id) this.props.lazyFetchGridTile(this.props.id);
-  }
-
-  componentDidUpdate(prevProps) {
-    if (this.props.api && this.props.id !== prevProps.id)
-      this.props.lazyFetchGridTile(this.props.id);
-  }
-
-  render() {
-    if (this.props.gridTileFetch) {
-      const { gridTileFetch } = this.props;
-      if (gridTileFetch.pending) {
-        return <Loader />;
-      } else if (gridTileFetch.rejected) {
-        return <Error message="Tile ID invalid" />;
-      } else if (gridTileFetch.fulfilled) {
-        return this.renderGridTile(gridTileFetch.value);
-      }
-    } else if (this.props.gridTile) {
-      return this.renderGridTile(this.props.gridTile);
-    } else if (!this.props.api) {
-      return this.renderGridTile(tiles.features.find(tile => tile.id === this.props.id));
+  if (api) {
+    if (isValidating) {
+      return <Loader />;
+    } else if (error) {
+      return <Error message="Error fetching grid tiles" />;
+    } else if (data) {
+      return <RenderGridTile gridTile={data} {...others} />;
     } else return null;
-  }
-}
+  } else if (gridTile) {
+    return <RenderGridTile gridTile={gridTile} {...others} />;
+  } else if (id) {
+    return <RenderGridTile gridTile={tiles.features.find(tile => tile.id === id)} {...others} />;
+  } else return null;
+};
 
 GridTile.propTypes = {
   id: PropTypes.string,
@@ -77,8 +64,4 @@ GridTile.defaultProps = {
   api: false,
 };
 
-export default connect(props => ({
-  lazyFetchGridTile: id => ({
-    gridTileFetch: `${API_URL}${props.id}/`,
-  }),
-}))(GridTile);
+export default GridTile;

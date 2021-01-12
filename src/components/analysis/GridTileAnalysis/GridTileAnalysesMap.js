022a0a095bef6fd36c7a8d6f6d810b14cc9c7c3b
George Moon <george@moon.org.nz>
2021-01-12 17:33:51 +1300

    Changing analysis logic to use JSX instead of HTML for popups

--- src/components/analysis/GridTileAnalysis/GridTileAnalysesMap.js
+++ src/components/analysis/GridTileAnalysis/GridTileAnalysesMap.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { GeoJSON, ScaleControl } from 'react-leaflet';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { ScaleControl, Popup, Polygon } from 'react-leaflet';
+import { GeoJSON as LeafletGeoJSON } from 'leaflet';
 
 import BaseMap from '../../map/BaseMap';
 
@@ -8,70 +10,64 @@ import tiles from '../../../assets/geo/tiles.json';
 import '../../map/GridTileMap.scss';
 
 /**
-  Generate colouring based on hours with/without kea.
- */
-const analysisTileStyle = feature => {
-  const hasKea = feature.hours_total.with_kea > 0;
-  const manyHours = feature.hours_total.total > 10;
-
-  const colour = hasKea ? '#df5206' : '#111111';
-
-  const fillOpacity = hasKea
-    ? 0.3 + (feature.hours_total.with_kea / feature.hours_total.total) * 0.4
-    : manyHours
-    ? 0.7
-    : 0.3;
-
-  return {
-    color: colour,
-    weight: 1,
-    opacity: 0.6,
-    fillOpacity: fillOpacity,
-  };
-};
-
-/**
   Map component for analysis of tiles.
  */
 class GridTileAnalysisMap extends Component {
-  gridTileOnEachFeature(feature, layer) {
-    layer.bindPopup(
-      `
-      <h2>${feature.id}</h2>
-      <dl class="form-row mb-2">
-        <dt class="col-8">Total</dt>
-        <dd class="col-4">${feature.hours_total.total}</dd>
-        <dt class="col-8">w/ kea</dt>
-        <dd class="col-4">${feature.hours_total.with_kea}</dd>
-      </dl>
-      <a href="/grid/${feature.id}">View</a>
-    `,
-      { direction: 'center', className: 'tile-popup', closeButton: false }
-    );
-  }
+  /**
+    Generate colouring based on hours with/without kea.
+   */
+  analysisTileStyle = gridTileAnalysis => {
+    const hasKea = gridTileAnalysis.hours_total.with_kea > 0;
+    const manyHours = gridTileAnalysis.hours_total.total > 10;
+
+    const colour = hasKea ? '#df5206' : '#111111';
+
+    const fillOpacity = hasKea
+      ? 0.3 + (gridTileAnalysis.hours_total.with_kea / gridTileAnalysis.hours_total.total) * 0.4
+      : manyHours
+      ? 0.7
+      : 0.3;
+
+    return {
+      color: colour,
+      weight: 1,
+      opacity: 0.6,
+      fillOpacity: fillOpacity,
+    };
+  };
+
+  /**
+    Create a Polygon for each gridTileAnalysis. Retrieve coordinates from raw GeoJSON, then convert (due to differing conventions).
+  */
+  createGridTile = gridTileAnalysis => (
+    <Polygon
+      positions={tiles.features
+        .find(tile => tile.id === gridTileAnalysis.id)
+        .geometry.coordinates.map(coordinate => LeafletGeoJSON.coordsToLatLngs(coordinate))}
+      key={gridTileAnalysis.id}
+      id={gridTileAnalysis.id}
+      {...this.analysisTileStyle(gridTileAnalysis)}
+    >
+      <Popup direction="center" className="tile-popup" closeButton={false}>
+        <h2>{gridTileAnalysis.id}</h2>
+        <dl className="form-row mb-2">
+          <dt className="col-8">Total</dt>
+          <dd className="col-4">{gridTileAnalysis.hours_total.total}</dd>
+          <dt className="col-8">w/ kea</dt>
+          <dd className="col-4">{gridTileAnalysis.hours_total.with_kea}</dd>
+        </dl>
+        <Link to={`/grid/${gridTileAnalysis.id}`}>View</Link>
+      </Popup>
+    </Polygon>
+  );
 
   render() {
     const { gridTileAnalyses } = this.props;
 
-    // Join the surveyedTiles with the stored tiles to obtain geospatial information
-    const surveyedTiles = Object.assign({}, tiles, {
-      features: gridTileAnalyses.map(gridTileAnalysis =>
-        Object.assign(
-          {},
-          gridTileAnalysis,
-          tiles.features.find(({ id }) => id === gridTileAnalysis.id)
-        )
-      ),
-    });
-
     return (
       <div className="GridTileMap">
         <BaseMap>
-          <GeoJSON
-            data={surveyedTiles}
-            onEachFeature={this.gridTileOnEachFeature}
-            style={analysisTileStyle}
-          />
+          {gridTileAnalyses.map(gridTileAnalysis => this.createGridTile(gridTileAnalysis))}
           <ScaleControl />
         </BaseMap>
       </div>
